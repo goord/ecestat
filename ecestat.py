9178d327ef5a21f352fb682e76a48307ba187c85
Oord van den <G.vandenOord@gmail.com>
2017-04-21 15:10:35 +0000

    Fixed ordereddict in python < 2.7 finally

--- ecestat.py
+++ ecestat.py
@@ -4,7 +4,7 @@ import sys
 import os.path
 import json
 import argparse
-if(sys.version_info < (2.7)):
+if(sys.version_info < (2,7)):
     from ordereddict import OrderedDict
 else:
     from collections import OrderedDict
